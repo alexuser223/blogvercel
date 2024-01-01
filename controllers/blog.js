b440d1e3a198ee7d978b91693e54a29361d3d2cc
Mohit22735 <mkpcpc28@gmail.com>
2024-01-01 15:15:00 +0530

    filter working

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,9 +1,12 @@
 const blogschema = require("../model/blogmodel");
 
 const homepage = async (req,res)=>{
+    const requestData = req.query;
+    // const para = {Unique_Id:Number(requestData.Unique_Id)};
 
     const myData = await blogschema.find(req.query);
-    // console.log("this is working",req.query)
+    console.log("this is working",req.query)
+    // console.log("this is working",para)
 
     res.status(200).json(myData)
 
